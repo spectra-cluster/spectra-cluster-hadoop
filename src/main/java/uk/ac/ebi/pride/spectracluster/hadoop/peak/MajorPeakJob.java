bc0b485f7a70f868844483c65abaaf100cf3ee38
Rui Wang <harry.r.wang@gmail.com>
2015-03-31 11:41:03 +0100

    change major peak job to take into account previous clustering results, also change reading input from spectra to cluster instead.

--- src/main/java/uk/ac/ebi/pride/spectracluster/hadoop/peak/MajorPeakJob.java
+++ src/main/java/uk/ac/ebi/pride/spectracluster/hadoop/peak/MajorPeakJob.java
@@ -7,14 +7,19 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.SequenceFileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
-import uk.ac.ebi.pride.spectracluster.hadoop.io.MGFInputFormat;
 import uk.ac.ebi.pride.spectracluster.hadoop.util.HadoopUtilities;
 
 /**
+ *
+ * MajorPeakJob performs incremental clustering on clusters that share the same peaks with high intensities
+ *
+ * NOTE: this job can take in multiple input directories, this is mainly used for
+ * loading previous clustering results
  *
  * @author Steve Lewis
  * @author Rui Wang
@@ -24,8 +29,8 @@ public class MajorPeakJob extends Configured implements Tool {
 
     @Override
     public int run(String[] args) throws Exception {
-        if (args.length != 5) {
-            System.err.printf("Usage: %s [generic options] <input directory> <output directory> <job name> <job configuration file> <counter file path>\n",
+        if (args.length < 5) {
+            System.err.printf("Usage: %s [generic options] <job name> <job configuration file> <counter file path> <output directory> <input directory> [multiple cluster result directory]\n",
                     getClass().getSimpleName());
             ToolRunner.printGenericCommandUsage(System.err);
             return -1;
@@ -34,22 +39,26 @@ public class MajorPeakJob extends Configured implements Tool {
         Configuration configuration = getConf();
 
         // load custom configurations for the job
-        configuration.addResource(args[3]);
+        configuration.addResource(args[1]);
 
         Job job = new Job(configuration);
-        job.setJobName(args[2]);
+        job.setJobName(args[0]);
         job.setJarByClass(getClass());
 
         // configure input and output path
-        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileInputFormat.addInputPath(job, new Path(args[4]));
+        if (args.length > 5) {
+            for (int i = 5; i < args.length; i++) {
+                FileInputFormat.addInputPath(job, new Path(args[i]));
+            }
+        }
 
-        Path outputDir = new Path(args[1]);
+        Path outputDir = new Path(args[3]);
         FileSystem fileSystem = outputDir.getFileSystem(configuration);
         FileOutputFormat.setOutputPath(job, outputDir);
 
         // input format
-        job.setInputFormatClass(MGFInputFormat.class);
-
+        job.setInputFormatClass(SequenceFileInputFormat.class);
         // output format
         job.setOutputFormatClass(SequenceFileOutputFormat.class);
 
@@ -68,7 +77,7 @@ public class MajorPeakJob extends Configured implements Tool {
 
         if (completion) {
             // output counters for the next job
-            String counterFileName = args[4];
+            String counterFileName = args[2];
             HadoopUtilities.saveCounters(fileSystem, counterFileName, job);
         }
 
